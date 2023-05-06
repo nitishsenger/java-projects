17004a58c5542366506ede6ea7df7eb8cdbadab0
Nitish <nitishsenger16@gmail.com>
2023-05-06 21:43:18 +0530

    transpose of a matrix

---
+++ transpose matrix.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int matrix[][] = new int[n][m];
+        
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The transpose of the matrix is: ");
+        //to print the transpose
+        for(int j=0 ; j<m ; j++){
+            for(int i=0 ; i<n ; i++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
