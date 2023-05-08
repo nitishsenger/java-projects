17a08966f30220807982fe47c9439957f711b16f
Nitish <nitishsenger16@gmail.com>
2023-05-08 22:40:48 +0530

    substring in java

---
+++ substring.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        String sentence = "My name is Tony Stark";
+        String name = sentence.substring(0,7);  //begining and ending
+        System.out.println(name);
+    }
+}
