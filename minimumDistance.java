c6d4da31751d8ea429293cefdba18f532bd837b2
Nitish <nitishsenger16@gmail.com>
2023-05-09 13:33:59 +0530

    updated

--- minimumDistance.java
+++ minimumDistance.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class minimumDistance{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int Ax = sc.nextInt();
@@ -9,6 +9,9 @@ public class Main{
         
         int D1 = (Ax * Ax) + (Ay * Ay);
         int D2 = (Bx * Bx) + (By * By);
+
+        System.out.println("D1 is "+D1);
+        System.out.println("D2 is "+D2);
         
         if(D1 < D2){
             System.out.println(D1);
