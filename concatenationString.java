d570137b7f37a9bd0a44b4da13b02dff75ad0d07
Nitish <nitishsenger16@gmail.com>
2023-05-14 23:29:30 +0530

    concatenation of strings

---
+++ concatenationString.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class concatenationString {
+    public static void main(String args[]){
+            String firstName = "Nitish";
+            String lastName = "Sengar";
+            String fullName = firstName+" "+lastName;
+            System.out.println(fullName);
+        }
+}  
