24e96dc246221d6d2ca1f3561f39316f76ec0e97
Nitish <nitishsenger16@gmail.com>
2023-05-15 22:59:01 +0530

    String builders

---
+++ stringBuilder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class stringBuilder {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder ("Tony");
+        System.out.println(sb);
+
+        //char at index 0
+        System.out.println(sb.charAt(0));
+
+        //set char at index 0
+        sb.setCharAt(0, 'P');
+        System.out.println(sb);
+    }
+    
+}
