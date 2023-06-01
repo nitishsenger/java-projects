b69f59a7840cc3257873989fef17f1284958e764
Nitish <nitishsenger16@gmail.com>
2023-06-01 23:59:24 +0530

    bubble sorting

--- getbit.java
+++ getbit.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class getbit {
     public static void main(String args[]){
-        int n = 16; //10000
-        int pos = 4;
+        int n = 5; //0101
+        int pos = 3;
         int bitMask = 1<<pos;
 
         if((bitMask & n) == 0){
