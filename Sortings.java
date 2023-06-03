f8335abc7c8d79276b2358838626aa68695fdc13
Nitish <nitishsenger16@gmail.com>
2023-06-03 23:53:33 +0530

    selection sort

--- Sortings.java
+++ Sortings.java
@@ -1,3 +1,30 @@
+// import java.util.*;
+// class Sortings{
+//     public static void printArray(int arr[]){
+//         for(int i=0 ; i<=arr.length ; i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String args[]){
+//         int arr[] = {7,8,3,1,2};
+
+//         for(int i=0 ; i<arr.length-1 ; i++){
+//             for(int j=0 ; j<arr.length-i-1 ; j++){
+//                 if(arr[j] > arr[j+1]){
+//                     //swap
+
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                 }
+//             }
+//         }
+        
+//         printArray(arr);
+//     }
+// }
+
 import java.util.*;
 
 
@@ -11,7 +38,7 @@ class Sortings {
 
 
    public static void main(String args[]) {
-       int arr[] = {7, 8, 1, 3, 2};
+       int arr[] = {7, 8, 1, 3, 2, 9 , 4};
 
 
        //bubble sort
