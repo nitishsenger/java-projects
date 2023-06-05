e18edc6e11099aafbd77613ced410b18f44c526e
Nitish <nitishsenger16@gmail.com>
2023-06-05 22:56:24 +0530

    repalce sting

---
+++ replaceString.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class replaceString {
+    public static void main(String args[]){
+
+        String str = "My name is khan and that picture is something";
+
+        String plainStr = str.replace("is","was");
+
+        System.out.println(plainStr);
+    }
+    
+}
